90e90388db33eb8b0b36acbd467895aa91cbecf1
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-10-30 14:43:51 -0300

    Update 1012-Area.py

---
+++ Python/Iniciante/1012-Area.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+a, b, c = [float(x) for x in input().split()]
+
+tr = (a * b)/2
+ci = 3.14159 * (c ** 2)
+t = ( (a+b)*c )/2
+q = b ** 2
+r = a * b
+
+print(
+f'''TRIANGULO: {tr:.3f}
+CIRCULO: {ci:.3f}
+TRAPEZIO: {t:.3f}
+QUADRADO: {q:.3f}
+RETANGULO: {r:.3f}'''
+      )
