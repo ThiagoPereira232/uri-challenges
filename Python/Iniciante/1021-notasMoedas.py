c2d1709e0fbbd9bd0b1d786807bd8a5be4f642e2
Thiago Pereira <pereira.tp.thiago@gmail.com>
2020-11-11 16:29:06 -0300

    1021

--- Python/Iniciante/1021-notasMoedas.py
+++ Python/Iniciante/1021-notasMoedas.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
-n = int(input())
+valor = float(input())
 
-a = n // 365
-n = n - a * 365
+notas = [100, 50, 20, 10, 5,2]
+moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
 
-m = n // 30
-n = n - m * 30
+print('NOTAS:')
+for nota in notas:
+    quantidade_nota = int(valor / nota)
+    print('{} nota(s) de R$ {:.2f}'.format(quantidade_nota, nota))
+    valor -= quantidade_nota * nota
 
-print(f'''{a} ano(s)
-{m} mes(es)
-{n} dia(s)''')
+print('MOEDAS:')
+for moeda in moedas:
+    quantidade_moeda = int(round(valor,2) / moeda)
+    print('{} moeda(s) de R$ {:.2f}'.format(quantidade_moeda, moeda))
+    valor -= quantidade_moeda * moeda
